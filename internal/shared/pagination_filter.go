7e24dfe6bd145a28b2e6d6d6534fdc344f6dc13d
alfianyulianto <alfianyulianto36@gmail.com>
2026-05-20 12:44:17 +0700

    fix(shared): correct validation rule in pagination filter struct

--- internal/shared/pagination_filter.go
+++ internal/shared/pagination_filter.go
@@ -1,8 +1,8 @@
 package shared
 
 type PaginationFilter struct {
-	Page           int    `json:"page"`
-	PerPage        int    `json:"per_page"`
-	OrderBy        string `json:"order_by"`
-	OrderDirection string `json:"order_direction"`
+	Page           int    `json:"page" validate:"required,number,gt=0"`
+	PerPage        int    `json:"per_page" validate:"required,number,gt=0,oneof=10 25 50 100"`
+	OrderBy        string `json:"order_by" validate:"required,oneof=created_at name"`
+	OrderDirection string `json:"order_direction" validate:"required,oneof=asc desc"`
 }
