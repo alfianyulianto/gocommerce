934ac4dbb1f4da60bc09a5f3354f53b946a9d710
alfianyulianto <alfianyulianto36@gmail.com>
2026-05-22 20:34:10 +0700

    feat(api): impelement graceful shutdown server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/alfianyulianto/gocommerce/config"
 	"github.com/alfianyulianto/gocommerce/internal"
@@ -18,6 +23,7 @@ func main() {
 	client := elasticsearch.NewClient(cfg, log)
 	app := infrastructure.New(cfg, log, db)
 	producer := kafka.NewProducer(cfg, log)
+	defer producer.Close()
 
 	internal.Bootstrap(&internal.BootstrapConfig{
 		App:      app,
@@ -27,6 +33,19 @@ func main() {
 		Config:   cfg,
 		Producer: producer,
 	})
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		<-quit
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := app.ShutdownWithContext(ctx); err != nil {
+			log.WithError(err).Error("Failed to shutdown app")
+		}
+		log.Info("Gracefully shutting down")
+	}()
 
 	err := app.Listen(fmt.Sprintf(":%d", cfg.App.Port))
 	if err != nil {
