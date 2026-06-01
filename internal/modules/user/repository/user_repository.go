1339014732f21f86e5e5a52fd9c903b8b89885b8
alfianyulianto <alfianyulianto36@gmail.com>
2026-06-01 10:56:26 +0700

    refactor(user): remove db parameter from repository interface

--- internal/modules/user/repository/user_repository.go
+++ internal/modules/user/repository/user_repository.go
@@ -14,30 +14,34 @@ type UserFilter struct {
 }
 
 type UserRepository interface {
-	Create(ctx context.Context, db *gorm.DB, entity *entity.User) error
-	Update(ctx context.Context, db *gorm.DB, entity *entity.User) error
-	Delete(ctx context.Context, db *gorm.DB, entity *entity.User) error
-	FindById(ctx context.Context, db *gorm.DB, entity *entity.User, id string) error
-	FindAll(ctx context.Context, db *gorm.DB, filter *UserFilter) ([]entity.User, int64, error)
+	Create(ctx context.Context, entity *entity.User) error
+	Update(ctx context.Context, entity *entity.User) error
+	Delete(ctx context.Context, entity *entity.User) error
+	FindById(ctx context.Context, entity *entity.User, id string) error
+	FindAll(ctx context.Context, filter *UserFilter) ([]entity.User, int64, error)
 }
 
 type userRepository struct {
 	shared.Repository[entity.User]
 }
 
-func NewUserRepository() UserRepository {
-	return &userRepository{}
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{
+		Repository: shared.Repository[entity.User]{
+			DB: db,
+		},
+	}
 }
 
-func (u *userRepository) FindAll(ctx context.Context, db *gorm.DB, filter *UserFilter) ([]entity.User, int64, error) {
+func (u *userRepository) FindAll(ctx context.Context, filter *UserFilter) ([]entity.User, int64, error) {
 	var users []entity.User
-	err := db.WithContext(ctx).Order(fmt.Sprintf("%s %s", filter.OrderBy, filter.OrderDirection)).Offset((filter.Page - 1) * filter.PerPage).Limit(filter.PerPage).Find(&users).Error
+	err := u.DB.WithContext(ctx).Order(fmt.Sprintf("%s %s", filter.OrderBy, filter.OrderDirection)).Offset((filter.Page - 1) * filter.PerPage).Limit(filter.PerPage).Find(&users).Error
 	if err != nil {
 		return nil, 0, err
 	}
 
 	var total int64
-	err = db.WithContext(ctx).Model(new(entity.User)).Count(&total).Error
+	err = u.DB.WithContext(ctx).Model(new(entity.User)).Count(&total).Error
 	if err != nil {
 		return nil, 0, err
 	}
