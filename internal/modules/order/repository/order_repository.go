d45d97678830782f88258fe277e0c8203c0d28c3
alfianyulianto <alfianyulianto36@gmail.com>
2026-06-01 17:20:34 +0700

    refactor(order): remove db parameter from repository interface

--- internal/modules/order/repository/order_repository.go
+++ internal/modules/order/repository/order_repository.go
@@ -9,16 +9,20 @@ import (
 )
 
 type OrderRepository interface {
-	Create(ctx context.Context, db *gorm.DB, order *entity.Order) error
-	Update(ctx context.Context, db *gorm.DB, order *entity.Order) error
-	Delete(ctx context.Context, db *gorm.DB, order *entity.Order) error
-	FindById(ctx context.Context, db *gorm.DB, order *entity.Order, id string) error
+	Create(ctx context.Context, order *entity.Order) error
+	Update(ctx context.Context, order *entity.Order) error
+	Delete(ctx context.Context, order *entity.Order) error
+	FindById(ctx context.Context, order *entity.Order, id string) error
 }
 
 type orderRepository struct {
 	shared.Repository[entity.Order]
 }
 
-func NewOrderRepository() OrderRepository {
-	return &orderRepository{}
+func NewOrderRepository(db *gorm.DB) OrderRepository {
+	return &orderRepository{
+		Repository: shared.Repository[entity.Order]{
+			DB: db,
+		},
+	}
 }
